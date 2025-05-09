f9ed37a05c86882bd0a499ec5478292f88ff9cf8
Adrio <adrio20@usp.br>
2025-05-09 12:01:32 -0300

    setup mocha test

---
+++ .mocharc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extension: ['ts'],
+  spec: ['./test/**/*'],
+  timeout: 2000,
+  require: ['ts-node/register'],
+};
---
+++ test/index.ts
@@ -0,0 +1,7 @@
+import { describe, it } from 'mocha';
+
+describe('Hello world', () => {
+  it('should pass this example test', () => {
+    console.log('Hello, world!');
+  });
+});
