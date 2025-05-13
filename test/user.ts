872e7d5200400a4e3a1cc8efb0540191fa039360
Adrio <adrio20@usp.br>
2025-05-13 19:21:23 -0300

    fix test to correcly check user insertion

--- test/user.ts
+++ test/user.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import bcrypt from 'bcrypt';
 import { expect } from 'chai';
-import { afterEach, describe, it } from 'mocha';
+import { describe, it } from 'mocha';
 import { prisma } from '../src/db';
 
 const PORT = process.env.PORT || 3001;
@@ -14,14 +15,27 @@ describe('POST /users', () => {
       password: 'password123',
       birthdate: '1990-01-01',
     };
+    const { password: plainPassword, ...userDataWithoutPassword } = userData;
 
     const response = await axios.post(`${BASE_URL}/users`, userData);
 
+    const createdUser = await prisma.user.findUnique({
+      where: { email: userData.email },
+    });
+    expect(createdUser).to.not.be.null;
+    const { id, password, ...userColumns } = createdUser!;
+    expect(id).to.be.gt(0);
+    expect(userColumns).to.deep.equal({
+      ...userDataWithoutPassword,
+      birthdate: new Date(userData.birthdate),
+    });
+    expect(await bcrypt.compare(userData.password, password)).to.be.true;
+
     expect(response.status).to.equal(201);
-    expect(response.data).to.include({
-      name: 'Test User',
-      email: 'Test.User@Mail.com',
-      birthdate: '1990-01-01T00:00:00.000Z',
+    expect(response.data).to.be.deep.eq({
+      id: id,
+      ...userColumns,
+      birthdate: new Date(userData.birthdate).toISOString(),
     });
   });
 });
